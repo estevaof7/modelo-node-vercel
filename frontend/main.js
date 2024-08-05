69ba9a7f8fe110b1788d28b192905c651d2b7ebe
Estêvão Ferreira <estevaofset@hotmail.com>
2024-08-05 11:27:31 -0300

    primeiro commit

---
+++ frontend/main.js
@@ -0,0 +1,1 @@
+console.log('estou no main.js');
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const router = require('./routes');
+
+const path = require('path');
+app.use(express.static(path.resolve(__dirname, 'frontend')));
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs'); 
+
+app.use(router);
+app.listen(3000, () => {
+    console.log('Servidor executando na porta 3000: http://localhost:3000')
+});
